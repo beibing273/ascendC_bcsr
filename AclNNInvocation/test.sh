4a0975f287d27ab3c7991a6539cc3d323e4efb7d
wuke_0814@qq.com <wuke_0814@qq.com>
2025-12-31 15:09:36 +0800

    暂时删除冲突之处

--- AclNNInvocation/test.sh
+++ AclNNInvocation/test.sh
@@ -44,7 +44,7 @@ function main {
     TIMER_PATH="src/timer.cpp"
     # 修改timer 输出目录
     # std::string filePath = "../output/" + category + ".txt";
-    sed -i "s|output/|$OUTPUT_DIR_NAME/|g" $TIMER_PATH
+    #sed -i "s|output/|$OUTPUT_DIR_NAME/|g" $TIMER_PATH
     # 定义输入输出目录
     INPUTS_DIR="../temp_input_copy"
     # INPUTS_DIR="../inputs_all"
@@ -121,13 +121,13 @@ function main {
 
     # 8.执行时间分析脚本
     # copy result to script dir for time analysis
-    cp "$OUTPUT_DIR/Bai.txt" ../scripts/
-    (
-        cd ../scripts/
-        python3 ./calculate_average_time.py
-    )
+    # cp "$OUTPUT_DIR/Bai.txt" ../scripts/
+    # (
+    #     cd ../scripts/
+    #     python3 ./calculate_average_time.py
+    # )
     # 9. 还原timer.cpp
-    sed -i "s|$OUTPUT_DIR_NAME/|output/|g" $TIMER_PATH
+    #sed -i "s|$OUTPUT_DIR_NAME/|output/|g" $TIMER_PATH
 }
 
 main
